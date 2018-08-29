8921d9911dba042c69ff408b2b98a36e590f3b9e
didadida262 <wzq7197@foxmail.com>
2018-08-29 23:47:32 +0800

    Add files via upload
    通过些许修改实现局域网内的两人聊天功能

---
+++ python网络编程-UDP-2.0/UDPServer.py
@@ -0,0 +1,13 @@
+from socket import *
+socketPort = 12000
+serverSocket =socket(AF_INET,SOCK_DGRAM)
+serverSocket.bind(('',socketPort))
+print "serverSocket is ready to receive:"
+
+while True:
+	message,clientAddress = serverSocket.recvfrom(2048)
+	print message
+
+	message2 = raw_input()
+	message2 = 'dida263: ' + message2
+	serverSocket.sendto(message2,clientAddress)
