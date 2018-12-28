67308e00a0e46790de62bcde79bd4ba31fc1ab99
didadida262 <wzq7197@foxmail.com>
2018-12-28 16:57:54 +0800

    Add files via upload

---
+++ 爬虫系列/touTiaoTest/touTiaoTest-更正版.py
@@ -0,0 +1,81 @@
+import requests
+from multiprocessing.pool import Pool
+from urllib.parse import urlencode
+import os
+from hashlib import md5
+
+
+
+
+def get_page(offset):
+	params = {
+        'offset': offset,
+        'format': 'json',
+        'keyword': '街拍',
+        'autoload': 'true',
+        'count': '20',
+        'cur_tab': '1',
+        'from': 'search_tab',
+        'pd':'synthesis'
+	}
+	base_url = 'https://www.toutiao.com/search_content/?'
+	url = base_url + urlencode(params)
+	try:
+		resp = requests.get(url)
+		if resp.status_code == 200:
+			return resp.json()
+		else:
+			return None
+	except requests.ConnectionError:
+		return None
+
+
+def get_image(json):
+	if json.get('data'):
+		data = json.get('data')
+		for item in data:
+			if item.get('cell_type') is not None:
+				continue
+			title = item.get('title')
+			images = item.get('image_list')
+			for image in images:
+				yield{
+				'title':title,
+				'image':'http:' + image.get('url')
+				}
+
+def save_image(item):
+	img_path = 'img' + os.path.sep + item.get('title')
+	if not os.path.exists(img_path):
+		os.makedirs(img_path)
+	try:
+		resp = requests.get(item.get('image'))
+		if resp.status_code == 200:
+			file_path = img_path + os.path.sep + '{file_name}.{file_suffix}'.format(
+				file_name = md5(resp.content).hexdigest(),
+				file_suffix = 'jpg')
+			if not os.path.exists(file_path):
+				with open(file_path,'wb') as f:
+					f.write(resp.content)
+			else:
+				print('Already download %s',file_path)
+	except requests.ConnectionError:
+		print('failed download')
+
+def main(offset):
+	json = get_page(offset)
+	for item in get_image(json):
+		save_image(item)
+
+
+
+
+GROUP_START = 0
+GROUP_END = 7
+
+if __name__ == '__main__':
+	pool = Pool()
+	groups = ([x*20 for x in range(GROUP_START,GROUP_END + 1)])
+	pool.map(main,groups)
+	pool.close()
+	pool.join()
